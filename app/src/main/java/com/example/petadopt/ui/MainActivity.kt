2343327f9385e52e8033603d452dc918b2006dcb
Ruben Kuilder <rubenkuilder@gmail.com>
2021-11-08 23:37:01 +0100

    Separate toolbar per fragment and added an image preview on the details fragment

--- app/src/main/java/com/example/petadopt/ui/MainActivity.kt
+++ app/src/main/java/com/example/petadopt/ui/MainActivity.kt
@@ -26,13 +26,6 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
-
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
-        val navController = navHostFragment.navController
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
